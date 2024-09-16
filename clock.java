52c3e061fa5cc278a6ffc2da6a4408928b61e77d
Akashr24 <akash.23cs008@sode-edu.in>
2024-09-16 21:08:55 +0530

    Add files via upload

--- clock.java
+++ clock.java
@@ -1,19 +1,13 @@
 public class clock{
       public static void main(String[] args){
-              double H;
-              double angle;
-              double min;
-              for(H=0;H<12;H++){
-              for(min=0;min<12;min++){
-              angle=(H*30)+min*(30-2.5);
-              if(angle>360)
-              {
-                angle=angle%360;
-              }
-              if(angle>180)
-              {
-                angle=360-angle;
-              }
-              System.out.println(H+":"+min*5+"-"+angle);
-              
- }}}}
+          int hour;
+          double angle;
+          int min;
+          String time;
+          for(hour=9;hour<9+12;hour++){
+            for(min=0;min<12;min++){
+              angle=((hour*30)+min*(30-2.5))%360;
+              time=hour+":"+min*5;
+              System.out.println(time+" - "+angle);}
+	 System.out.println();
+}}}
